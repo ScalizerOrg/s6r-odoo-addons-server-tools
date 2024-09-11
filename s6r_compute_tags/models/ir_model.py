06c8a6f09d7acc7536391e1452cd4669f6e55435
deJong <alban@scalizer.fr>
2024-09-11 11:06:41 +0200

    [IMP] s6r_compute_tags: res is now an empty list and can stay empty after safe_eval

--- s6r_compute_tags/models/ir_model.py
+++ s6r_compute_tags/models/ir_model.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Scalizer (https://www.scalizer.fr)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from odoo import models, api, Command
-from odoo.tools.safe_eval import safe_eval
+from odoo.tools.safe_eval import safe_eval, datetime
 from odoo.addons.base.models.ir_model import SAFE_EVAL_BASE
 
 
@@ -24,9 +24,10 @@ class BaseModel(models.AbstractModel):
 
     def _apply_tags(self, tag_ids, vals):
         for tag in tag_ids:
-            res = False
-            local_dict = {'self': self, 'res': res, 'vals': vals}
-            safe_eval(tag.compute_tags_method, SAFE_EVAL_BASE, local_dict, mode='exec', nocopy=True)
-            res = local_dict['res']
-            tags = self.env[tag.tag_field_id.relation].search([('name', 'in', res)])
-            vals[tag.tag_field_id.name] = [Command.set(tags.ids)]
+            res = []
+            global_dict = {'self': self, 'res': res, 'vals': vals, 'datetime': datetime}
+            safe_eval(tag.compute_tags_method, global_dict, mode='exec', nocopy=True)
+            res = global_dict['res']
+            if res:
+                tags = self.env[tag.tag_field_id.relation].search([('name', 'in', res)])
+                vals[tag.tag_field_id.name] = [Command.set(tags.ids)]
